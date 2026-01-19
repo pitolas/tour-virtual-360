be409c6814439759e2a001243141ae716d7b0133
pitolas <167545569+pitolas@users.noreply.github.com>
2026-01-19 11:27:20 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,461 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-trevo",
+      "name": "Trevo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": 0.003638286300009952,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3806988498146566,
+          "pitch": 0.02970681889580362,
+          "rotation": 0,
+          "target": "4-vista-para-pedra-e-sede"
+        },
+        {
+          "yaw": 2.980688760300092,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "1-parquinho"
+        },
+        {
+          "yaw": -2.5838095860013865,
+          "pitch": 0.05132902171894571,
+          "rotation": 0,
+          "target": "2-bebedouro"
+        },
+        {
+          "yaw": -0.6994837172536954,
+          "pitch": -0.14176667624747452,
+          "rotation": 0,
+          "target": "3-pedra-da-cebola"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-parquinho",
+      "name": "Parquinho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0164756493437643,
+          "pitch": 0.08875741214477806,
+          "rotation": 0,
+          "target": "2-bebedouro"
+        },
+        {
+          "yaw": 1.109690870642,
+          "pitch": 0.05040165843725575,
+          "rotation": 0,
+          "target": "0-trevo"
+        },
+        {
+          "yaw": 1.4219131548548543,
+          "pitch": -0.05851365871488312,
+          "rotation": 0,
+          "target": "4-vista-para-pedra-e-sede"
+        },
+        {
+          "yaw": 0.5866549531864571,
+          "pitch": -0.23649872985472165,
+          "rotation": 0,
+          "target": "3-pedra-da-cebola"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-bebedouro",
+      "name": "Bebedouro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.313750431442678,
+          "pitch": 0.014573011799310365,
+          "rotation": 0,
+          "target": "1-parquinho"
+        },
+        {
+          "yaw": 1.680862454887185,
+          "pitch": -0.02482627106235924,
+          "rotation": 0,
+          "target": "0-trevo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-pedra-da-cebola",
+      "name": "Pedra da Cebola",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3828432021335892,
+          "pitch": 0.16411340671027297,
+          "rotation": 0,
+          "target": "4-vista-para-pedra-e-sede"
+        },
+        {
+          "yaw": 0.0516887131454844,
+          "pitch": 0.05156123510165678,
+          "rotation": 0,
+          "target": "0-trevo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vista-para-pedra-e-sede",
+      "name": "Vista para pedra e Sede",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.787860890537509,
+          "pitch": -0.10154389193261082,
+          "rotation": 0,
+          "target": "3-pedra-da-cebola"
+        },
+        {
+          "yaw": 1.3918934595387213,
+          "pitch": 0.025691974892326996,
+          "rotation": 0,
+          "target": "0-trevo"
+        },
+        {
+          "yaw": 0.20795793585589095,
+          "pitch": 0.06465914912764248,
+          "rotation": 0,
+          "target": "5-campo-de-baseball"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-campo-de-baseball",
+      "name": "Campo de baseball",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2777488122333125,
+          "pitch": 0.00643026198272878,
+          "rotation": 0,
+          "target": "7-parquinho-2"
+        },
+        {
+          "yaw": -0.4564854221442811,
+          "pitch": -0.04180148320081045,
+          "rotation": 0,
+          "target": "8-rea-de-ioga"
+        },
+        {
+          "yaw": 0.6906214949294665,
+          "pitch": 0.03446869938251851,
+          "rotation": 0,
+          "target": "4-vista-para-pedra-e-sede"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-rea-de-lazer",
+      "name": "Área de lazer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7849713686408526,
+          "pitch": -0.02013346229522206,
+          "rotation": 0,
+          "target": "8-rea-de-ioga"
+        },
+        {
+          "yaw": -2.530113395567275,
+          "pitch": 0.08869843938977517,
+          "rotation": 0,
+          "target": "5-campo-de-baseball"
+        },
+        {
+          "yaw": 2.965797940579649,
+          "pitch": -0.032005042520008686,
+          "rotation": 0,
+          "target": "4-vista-para-pedra-e-sede"
+        },
+        {
+          "yaw": 0.2508179093659848,
+          "pitch": 0.10139796485913166,
+          "rotation": 0,
+          "target": "7-parquinho-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-parquinho-2",
+      "name": "Parquinho 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5510463112638746,
+          "pitch": 0.021940253725469816,
+          "rotation": 0,
+          "target": "8-rea-de-ioga"
+        },
+        {
+          "yaw": 1.7813153476919243,
+          "pitch": 0.05187702127802396,
+          "rotation": 0,
+          "target": "5-campo-de-baseball"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-rea-de-ioga",
+      "name": "Área de Ioga",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1472,
+      "initialViewParameters": {
+        "yaw": -1.2024141311774876,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.488451198493891,
+          "pitch": 0.03459203482653628,
+          "rotation": 0,
+          "target": "7-parquinho-2"
+        },
+        {
+          "yaw": 1.7168302401021434,
+          "pitch": 0.07303297694156008,
+          "rotation": 0,
+          "target": "6-rea-de-lazer"
+        },
+        {
+          "yaw": 1.0947031502970859,
+          "pitch": 0.04634232108572078,
+          "rotation": 0,
+          "target": "5-campo-de-baseball"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
